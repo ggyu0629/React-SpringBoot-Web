8ae912af0af92c1e4d33f733cefa9ba9cd1056df
ggyu0629 <ksg1885@naver.com>
2023-07-04 17:39:07 +0900

    Header 버튼 생성 및 주소변환

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div>
-      컨텐츠 요소가 표시됩니다.
+      컨텐츠 요소가 표시됩니다용.
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+  <>
+    <Header/>
+    <App/>
+    <Footer/>
+  </>
 );
